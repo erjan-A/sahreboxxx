3ac5c3ed1977ac9910c559bdbaa7e7d905ad6613
Erjan <ekazhman@gmail.com>
2014-02-06 17:01:20 +0600

    initial commit

---
+++ sharebox/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sharebox::Application.config.secret_key_base = '9ad1df5f23e5242be896389403485657df36228493f28beb34c5136bb09ac7f4b18866ab109c09883eced09b2a07106802bf36c3636062c80e6d333dafe1548b'
